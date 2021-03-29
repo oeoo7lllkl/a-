80fd652603ec1d7b1e89f804885cb7b984501315
Your Name <olelolok7l@naver.com>
2021-03-29 14:02:14 +0900

    -

--- CHAGEcOLOR/index.php
+++ CHAGEcOLOR/index.php
@@ -10,7 +10,7 @@ $color="a56878";
 include "!include/header.php"?>
 
 <?php 
-//include "!include/삭제해.html"
+// include "!include/____삭제해.html"
 ?>
 
 <div id="RefreshPage" onclick="location.href='.'">
@@ -21,8 +21,8 @@ include "!include/header.php"?>
     <?php include "!include/!menu/!LEFT/cloth.html"?>
         <?php $cloth="index";
             if(isset($_POST["cloth"])){$cloth=$_POST["cloth"];
-                $_SESSION[$pageid.'cloth']=$cloth;}else if(isset($_SESSION[$pageid.'cloth'])){
-                $cloth=$_SESSION[$pageid.'cloth'];}
+                $_SESSION[$_pageid.'cloth']=$cloth;}else if(isset($_SESSION[$_pageid.'cloth'])){
+                $cloth=$_SESSION[$_pageid.'cloth'];}
         ?>
     <?php include "!include/man.php"?>
     <?php include "!include/!menu/!right/".$cloth.".html"?>
