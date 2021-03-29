977c172f1c62c7b0fa32a322872e1510846a8e00
Your Name <olelolok7l@naver.com>
2021-03-29 23:32:11 +0900

    팔목,바지,신발 넓이 변경해야됨

--- CHAGEcOLOR/!CSS/man.php
+++ CHAGEcOLOR/!CSS/man.php
@@ -49,22 +49,23 @@
     background-color: rgb(135, 141, 141);
 }
 #percent{
-    width: 199;height: 99.5;
+    width: 183;height: 91.5;
     /*width=2*height*/
     margin:0 auto;
     position: relative;
-    top: 50;
+    top: 65;
 }
 #man{
+    background:red;
     background-color: /*배경*/transparent;
-    width: /*넓이*/85%;
+    width: /*넓이*/83%;
     height: /*높이*/662%;
     /*초기비율: width:176,height:662*/
     margin:0 auto;
 }
 #head{
     background-color: #ebd6c0;
-    width: 31%;height: 13%;
+    width: 34%;height: 13.5%;
     margin:0 auto;
 }
     .hat{
@@ -79,10 +80,10 @@
             background-color: black;
         }
     .face{
-        height: 15%;
+        height: 20%;
     }
         .glasses{
-            height: 100%;
+            height: 85%;
             position: relative;
             top: 20%;
             display: flex;
@@ -96,13 +97,14 @@
                 rgb(211, 180, 8);
             }
                 .side{
-                    width: 10%;height: 0%;
+                    width: 5%;height: 0%;
                     position: relative;
                     top: 50%;
                 }
                 .glass.line{
-                    width: 35%;height: 100%;
-                    border:1px solid black;
+                    width: 40%;height: 100%;
+                    border:1px solid 
+                    rgb(211, 180, 8);
                 }
                 .nose{
                     width: 10%;height: 0%;
@@ -110,13 +112,13 @@
                     top: 25%;
                 }
     .mask{
-        height: 41%;
+        height: 36%;
         background-color: /*마스크*/white;
     }
 #neck-center{
     margin:0 auto;
-    width: 29%;
-    height: 6%;
+    width: 35%;
+    height: 5.5%;
 }
     #neck{
         display: flex;
@@ -127,13 +129,13 @@
             height: 100%;
         }
             .neck.jacket{
-                width: 25%;height: 83%;
+                width: 15%;height: 83%;
                 background-color: /*목깃자켓*/<?=$color_jacket?>;
             }
             .neck.skin{
                 background-color: 
                 #ebd6c0;
-                width: 62%;
+                width: 70%;
             }
                 .neck.skin #box1{
                     background-color: transparent;
@@ -146,6 +148,7 @@
 #body{
     display: flex;
     height: 81%;
+    background:red;
 }
     #side{
         margin: 0ch;
@@ -157,7 +160,7 @@
             width: 100%;height:21.25%;
         }
         .arm.first{
-            width: 99.5%;height: 21.25%;
+            width: 99.5%;height: 22%;
             background-color: /*자켓*/<?=$color_jacket?>;
         }
         .arm.second{
@@ -167,11 +170,13 @@
             /*팔중
             70%;
             팔하*/
-            height: 38.75%;
+            height: 42%;
             background-color: /*팔자켓*/<?=$color_jacket?>;
+            background:white;
         }
             .right.arm{
                 margin-right: auto;
+                    border-right:0.5px solid black;
             }
                 .right.arm.second{
                     margin-right: auto;
@@ -183,6 +188,7 @@
                 }
             .left.arm{
                 margin-left: auto;
+                    border-left:0.5px solid black;
             }
                 .left.arm.second{
                     margin-left: auto;
@@ -194,7 +200,7 @@
                 }
         .hand{
             background-color: #ebd6c0;
-            width: 80%;height: 18.75%;
+            width: 80%;height: 21%;
         }
             .right.hand{
                 margin-right: auto;
@@ -212,7 +218,7 @@
     }
         #top{
             display: flex;
-            height: 40.7%;
+            height: 42%;
         }
             #jacket{
                 background-color: /*자켓*/<?=$color_jacket?>;
@@ -240,16 +246,16 @@
 
         #pants-top{
             background-color: <?=$color_pants?>;
-            width: 98%;height: 10.5%;
+            width: 98%;height: 8.2%;
             margin:0 auto;
         }
         #pants-bottom{
             display: flex;
             width: 98%;height: 
             /*발상*/
-            39.5%;
+            40.5%;
             /*발중
-            37.6%;
+            38.6%;
             발하*/
             margin:0 auto;
         }
