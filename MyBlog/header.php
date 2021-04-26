6dfee686e0b3bfdf087271141e20453a84429f71
Your Name <olelolok7l@naver.com>
2021-04-26 22:52:37 +0900

    -

---
+++ MyBlog/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My Blog</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,100&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body onkeypress="keypress()">
+    
+    <nav>   
+        <div class="wrapper">
+            <a href="index.php"><img src="img/logo-white.png" alt="Blogs logo"></a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="discover.php">About Us</a></li>
+                <li><a href="blog.php">Find Blogs</a></li>
+                <li><a href="signup.php">Sign Up</a></li>
+                <li><a href="login.php">Log in</a></li>
+            </ul>   
+        </div>
+    </nav>
+    
+<div class="wrapper">
---
+++ MyBlog/index.php
@@ -0,0 +1,30 @@
+<?php
+	include_once 'header.php';
+?>
+
+    <section class="index-intro">
+        <h1>This Is An Introduction</h1>
+        <p>Here is an important paragraph that explains the purpose of the website</p>
+    </section>
+
+    <section class="index-categories">
+        <h2>Some Basic Categories</h2>
+        <div class="index-categories-list">
+            <div>
+                <h3>Fun Stuff</h3>
+            </div>
+            <div>
+                <h3>Serious Stuff</h3>
+            </div>
+            <div>
+                <h3>Exciting Stuff</h3>
+            </div>
+            <div>
+                <h3>Boring Stuff</h3>
+            </div>
+        </div>
+    </section>
+
+<?php
+	include_once 'footer.php';
+?>
